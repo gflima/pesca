a5b6134488537e44a421dff5e4005d930ae4a3bf
Guilherme Lima <Guilherme.Lima@ibm.com>
2021-04-08 18:21:51 -0300

    src: Add support for primed constants

--- src/PSequent.hs
+++ src/PSequent.hs
@@ -54,7 +54,9 @@ pTerm2 =
 
 pScheme = longestOfSome pCapital ... longestOfMany pDigit *** junct2 -- 1+ capital letters, 1+ digits
 pPred   = pCapital ... longestOfSome pSmall *** junct  -- capit. with 1+ small 
-pConst  = pSmall   ... longestOfSome pSmall *** junct  -- 2+ small letters
+pConst  = pSmall ... pVar *** junct
+           |||
+          pSmall   ... longestOfSome pSmall *** junct  -- 2+ small letters
            |||
           longestOfSome pDigit                         -- or 1+ digits
 pVar    = pSmall   ... longestOfMany (literal '\'') *** junct
